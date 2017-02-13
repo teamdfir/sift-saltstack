8afea8adf0c87eda3f9b327828673a27bb9ea3ba
Erik Kristensen <erik@erikkristensen.com>
2017-02-12 19:33:20 -0700

    working on tests and a reorganization of states

---
+++ tests/packages.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+for STATE in `ls sift/packages`; do
+  docker run -t --rm --name="sift-package-$STATE" sift-salt-tester \
+    salt-call --local --retcode-passthrough --state-output=mixed \
+      state.sls sift.packages.`basename -s .sls ${STATE}` || exit 1
+done

539409b814f502cf043a5c55fff50ebcf8f13f62
Erik Kristensen <erik@erikkristensen.com>
2017-02-13 18:00:46 -0700

    fixing script

--- tests/changed-files.sh
+++ tests/changed-files.sh
@@ -3,7 +3,7 @@
 for FILE in `git diff-tree --no-commit-id --name-only -r ${TRAVIS_COMMIT}`; do
   STATE=$(echo $FILE | sed "s/.sls//g" | sed "s/\//./g")
   docker run -t --rm --name="sift-package-${STATE}" sift-salt-tester \
-    salt-call --local --retcode-passthrough --state-output=mixed state.sls ${STATE} || exit 1"
+    salt-call --local --retcode-passthrough --state-output=mixed state.sls ${STATE} || exit 1
 done
 
 

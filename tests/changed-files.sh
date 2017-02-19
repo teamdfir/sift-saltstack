b7dc5d1fd3b844fc55dfe743956b82e5fe4075dc
Erik Kristensen <erik@erikkristensen.com>
2017-02-19 12:15:47 -0700

    only test sls files and not init.sls

--- tests/changed-files.sh
+++ tests/changed-files.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "States To Test:"
-echo "`git diff-tree --no-commit-id --name-only -r ${TRAVIS_COMMIT} | grep "sift/" | grep -v "init.sls"`"
+echo "`git diff-tree --no-commit-id --name-only -r ${TRAVIS_COMMIT} | grep "sift/" | grep ".sls$" | grep -v "init.sls"`"
 
-for FILE in `git diff-tree --no-commit-id --name-only -r ${TRAVIS_COMMIT} | grep "sift/" | grep -v "init.sls"`; do
+for FILE in `git diff-tree --no-commit-id --name-only -r ${TRAVIS_COMMIT} | grep "sift/" | grep ".sls$" | grep -v "init.sls"`; do
   STATE=$(echo $FILE | sed "s/.sls//g" | sed "s/\//./g")
   echo "Testing ${STATE}"
   docker run -t --rm --name="sift-package-${STATE}" -v `pwd`/sift:/srv/salt/sift sift-salt-tester \

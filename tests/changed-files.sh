0a863ca36015dd0b2463d8a3813a50a995fb3d64
Erik Kristensen <erik@erikkristensen.com>
2017-02-19 19:46:51 -0700

    improving the test/changed-files.sh file

--- tests/changed-files.sh
+++ tests/changed-files.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-echo "States To Test:"
-echo "`git diff-tree --no-commit-id --name-only -r ${TRAVIS_COMMIT} | grep "sift/" | grep ".sls$" | grep -v "init.sls"`"
+CHANGED_FILES=$(git diff-tree --no-commit-id --name-status -r ${TRAVIS_COMMIT} | grep -v "^D" | awk '{ print $2 }' | grep ".sls$" | grep -v "init.sls" | grep "sift/")
 
-for FILE in `git diff-tree --no-commit-id --name-only -r ${TRAVIS_COMMIT} | grep "sift/" | grep ".sls$" | grep -v "init.sls"`; do
+echo "States To Test:"
+echo "${CHANGED_FILES}"
+echo ""
+
+for FILE in "${CHANGED_FILES}"; do
   STATE=$(echo $FILE | sed "s/.sls//g" | sed "s/\//./g")
   echo "Testing ${STATE}"
-  docker run -t --rm --name="sift-package-${STATE}" -v `pwd`/sift:/srv/salt/sift sift-salt-tester \
+  docker run -t --rm -v `pwd`/sift:/srv/salt/sift sift-salt-tester \
     salt-call --local --retcode-passthrough --state-output=mixed state.sls ${STATE} || exit 1
 done
 

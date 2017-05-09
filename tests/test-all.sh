b3e2ba249bb38b9ea92fd2a40265f613cc4aa364
Erik Kristensen <erik@erikkristensen.com>
2017-05-08 19:23:28 -0600

    fixing test-all script

--- tests/test-all.sh
+++ tests/test-all.sh
@@ -3,4 +3,4 @@
 SIFT_VERSION=${1:-stable}
 
 docker run -it --rm --name="sift-salt-test-all" -v `pwd`/sift:/srv/salt/sift sansdfir/sift-salt-tester \
-  salt-call --local --retcode-passthrough --state-output=mixed state.sls sift pillar="{sift_version: ${SIFT_VERSION}}"
+  salt-call --local --retcode-passthrough --state-output=mixed state.sls sift.vm pillar="{sift_version: ${SIFT_VERSION}}"

1f6aee4aebfe834c14e2640e232209c8dcce1ea2
Erik Kristensen <erik@erikkristensen.com>
2017-06-19 22:56:32 -0500

    fixing path to scripts

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,21 +6,21 @@ if [ "x$TRAVIS_TAG" != "x" ]; then
   echo "Detected Event: Tag"
   echo " > TAG: ${TRAVIS_TAG}"
   echo ""
-  ./test-all-publish.sh || exit 1
+  ./scripts/test-all-publish.sh || exit 1
   exit 0
 fi
 
 if [ "$TRAVIS_EVENT_TYPE" != "cron" ]; then
   echo "Detected Event: Push or PR"
   echo ""
-  ./changed-files.sh || exit 1
+  ./scripts/changed-files.sh || exit 1
   exit 0
 fi
 
 if [ "$TRAVIS_EVENT_TYPE" = "cron" ]; then
   echo "Detected Event: CRON"
   echo ""
-  ./tests/test-all.sh || exit 1
+  ./scripts/tests/test-all.sh || exit 1
   exit 0
 fi
 

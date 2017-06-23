30e47e56515d043920a3c90eb7dbf6eb93441cac
Erik Kristensen <erik@erikkristensen.com>
2017-06-23 13:31:46 -0400

    Revamp tests (#11)
    * improving tests
    
    * fixing path to scripts
    
    * improving tests
    
    * have travis ci publish the draft so long as tests pass
    
    * ensure the test all tests the right thing
    
    * fixing log output [skip ci]

--- tests/test-all.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-SIFT_VERSION=${1:-stable}
-
-docker run -it --rm --name="sift-salt-test-all" -v `pwd`/sift:/srv/salt/sift sansdfir/sift-salt-tester \
-  salt-call --local --retcode-passthrough --state-output=mixed state.sls sift.vm pillar="{sift_version: ${SIFT_VERSION}}"

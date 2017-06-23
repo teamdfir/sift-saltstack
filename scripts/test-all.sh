d0e1706fb2cbbe4b1113b33509dbbbe8da7efc7c
Erik Kristensen <erik@erikkristensen.com>
2017-06-23 12:25:14 -0400

    ensure the test all tests the right thing

--- scripts/test-all.sh
+++ scripts/test-all.sh
@@ -2,5 +2,5 @@
 
 SIFT_VERSION=${1:-stable}
 
-docker run -it --rm --name="sift-salt-test-all" -v `pwd`/sift:/srv/salt/sift sansdfir/sift-salt-tester \
-  salt-call --local --retcode-passthrough --state-output=mixed state.sls sift.vm pillar="{sift_version: ${SIFT_VERSION}}"
+docker run -it --rm --name="sift-package-${STATE}" -v `pwd`/sift:/srv/salt/sift sansdfir/sift-salt-tester \
+  salt-call -l debug --local --retcode-passthrough --state-output=mixed state.sls sift.vm pillar="{sift_version: ${SIFT_VERSION}}"

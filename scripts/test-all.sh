103887a462a82a17d337c830e82cc37c2b6265d4
Erik Kristensen <erik@erikkristensen.com>
2017-06-23 13:31:17 -0400

    fixing log output [skip ci]

--- scripts/test-all.sh
+++ scripts/test-all.sh
@@ -3,4 +3,4 @@
 SIFT_VERSION=${1:-stable}
 
 docker run -it --rm --name="sift-package-${STATE}" -v `pwd`/sift:/srv/salt/sift sansdfir/sift-salt-tester \
-  salt-call -l debug --local --retcode-passthrough --state-output=mixed state.sls sift.vm pillar="{sift_version: ${SIFT_VERSION}}"
+  salt-call -l info --local --retcode-passthrough --state-output=mixed state.sls sift.vm pillar="{sift_version: ${SIFT_VERSION}}"

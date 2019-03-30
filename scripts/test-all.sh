ec6eead93df49af6567daeb2483dc07f6ce162f0
Erik Kristensen <erik@erikkristensen.com>
2019-03-30 10:43:10 -0600

    some 18.04 stuff

--- scripts/test-all.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-SIFT_VERSION=${1:-stable}
-
-docker run -it --rm --name="sift-package-${STATE}" -v `pwd`/sift:/srv/salt/sift --cap-add SYS_ADMIN sansdfir/sift-salt-tester \
-  salt-call -l info --local --retcode-passthrough --state-output=mixed state.sls sift.vm pillar="{sift_version: ${SIFT_VERSION}}"

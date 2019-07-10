44daee2fd742335caaf0cf2ae6cf45c9de4ee10e
Erik Kristensen <erik@erikkristensen.com>
2019-07-10 13:18:54 -0400

    updating .ci scripts

--- .ci/test-state.sh
+++ .ci/test-state.sh
@@ -6,4 +6,4 @@ DISTRO=${DISTRO:="bionic"}
 STATE=$1
 
 docker run -it --rm --name="sift-state-${STATE}" -v `pwd`/sift:/srv/salt/sift --cap-add SYS_ADMIN teamdfir/sift-saltstack-tester:${DISTRO} \
-  salt-call -l debug --local --retcode-passthrough --state-output=mixed state.sls ${STATE} pillar="{remnux_user: root}"
+  salt-call -l debug --local --retcode-passthrough --state-output=mixed state.sls ${STATE} pillar="{sift_user: root}"

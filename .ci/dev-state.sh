033a6ab648415a59239eb717a79762a23075d6ee
Erik Kristensen <erik@erikkristensen.com>
2023-01-31 19:05:51 -0700

    chore: update scripts for testing purposes

--- .ci/dev-state.sh
+++ .ci/dev-state.sh
@@ -3,7 +3,8 @@
 set -x
 
 DISTRO=${DISTRO:="focal"}
+SALT=${SALT:="3005"}
 STATE=$1
 
-docker run -it --rm --name="sift-state-${STATE}" -p 8080:80 -v `pwd`/sift:/srv/salt/sift --cap-add SYS_ADMIN teamdfir/sift-saltstack-tester:${DISTRO} \
+docker run -it --rm --name="sift-state-${STATE}" -p 8080:80 -v `pwd`/sift:/srv/salt/sift --cap-add SYS_ADMIN ghcr.io/teamdfir/sift-saltstack-tester:${SALT}-${DISTRO} \
   /bin/bash
